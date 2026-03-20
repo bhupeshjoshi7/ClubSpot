887f082ed2cdb34860039ea4e531b1fe673ff308
bhupeshjoshi7 <bhupeshjoshi2002@gmail.com>
2026-03-20 08:12:35 +0530

    bypass render spam blocks with port 587

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -9,8 +9,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, // Use SSL
+    port: 587,
+    secure: false, // Use STARTTLS instead of implicit TLS (Bypasses Render's port 465 block)
     auth: {
         user: process.env.EMAIL_USER || '', // e.g. clubspot@gmail.com
         pass: process.env.EMAIL_PASS || '', // App password
